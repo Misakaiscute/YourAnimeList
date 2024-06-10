fa55284dcffa2a6e15c666c8e83425d8b359894e
Misakaiscute <horchy.hunorpihe@gmail.com>
2024-06-10 16:44:53 +0200

    intitial commit

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,11 +11,7 @@ class DashboardController extends Controller
     {
         $response = Http::post('http://127.0.0.1:8000/api/dashboard');
 
-        dd($response);
-
         $data = json_decode($response);
-
-        dd($data);
 
         return view('profile.dashboard', ['user' => $data]);
     }
